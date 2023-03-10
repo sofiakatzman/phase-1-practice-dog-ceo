ab8f581e748778720e4d4f94d4f7476f56cc9e8e
Sofia Katzman <katzmansof@gmail.com>
2023-03-09 22:52:42 -0500

    partially completed lab - need to create filter function to only display names by first letter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,51 @@
-console.log('%c HI', 'color: firebrick')
+console.log('%c HELLO', 'color: firebrick')
+
+function ceo(){
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(json => renderImg(json))
+}
+
+function loadBreeds(){
+    const breedUrl = "https://dog.ceo/api/breeds/list/all"
+    fetch(breedUrl)
+    .then(resp => resp.json())
+    .then(json => renderBreeds(json))
+}
+
+function renderImg(images) {
+    const location = document.querySelector('div')
+    images.message.forEach(Image => {
+      const img = document.createElement('img');
+      img.src = Image;
+      location.appendChild(img)
+    });
+  }
+
+
+function renderBreeds(breeds) {
+  allBreeds = Object.keys(breeds.message)
+  allBreeds.forEach(breedName => {
+    const ul = document.createElement('li')
+    const location = document.getElementById('dog-breeds')
+    ul.innerHTML = breedName;
+    location.appendChild(ul)
+    console.log(breedName)
+   })
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    ceo()
+    loadBreeds()
+  })
+
+
+let liBreedSelect = document.getElementById("dog-breeds")
+liBreedSelect.addEventListener('click', (evt) => {
+  evt.target.style.color = "#ff69b4"
+})
+
+
+
